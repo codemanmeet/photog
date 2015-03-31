40dcf6280bbc48fd20b4866b943510333a189b7d
Manmeet Saluja <manmeet.saluja94@gmail.com>
2015-03-31 15:46:07 -0700

    First commit

---
+++ CustomObject.m
@@ -0,0 +1,9 @@
+//
+//  CustomObject.m
+//  Photog
+//
+//  Created by Manmeet Saluja on 25/3/15.
+//  Copyright (c) 2015 Manmeet Saluja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
